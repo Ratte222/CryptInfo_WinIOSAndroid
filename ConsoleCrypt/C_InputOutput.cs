c2bcfaa7de7f39f17a83eec39d3cbcb7e1c35823
Ratte222 <Artur21111999.o@gmail.com>
2021-01-31 14:48:47 +0200

    Fix decrypt file

--- ConsoleCrypt/C_InputOutput.cs
+++ ConsoleCrypt/C_InputOutput.cs
@@ -122,6 +122,8 @@ namespace ConsoleCrypt
         }
         public I_INPUTOUTPUTMESSAGE CryptFile(string key)
         {
+            StreamReader srDecrypt = null;
+            StreamWriter swCrypt = null;
             try
             {
                 if (appSettings == null)
@@ -135,26 +137,18 @@ namespace ConsoleCrypt
                 if (String.IsNullOrWhiteSpace(key) || String.IsNullOrEmpty(key))
                     return I_INPUTOUTPUTMESSAGE.KeyIsNull;
                 string content;
-                using (StreamReader srDecrypt = new StreamReader(appSettings.DirDecryptFile, Encoding.UTF8))
+                srDecrypt = new StreamReader(appSettings.DirDecryptFile, Encoding.UTF8);
+                swCrypt = new StreamWriter(appSettings.DirCryptFile, false, Encoding.UTF8);
+                while (true)
                 {
-                    using (StreamWriter swCrypt = new StreamWriter(appSettings.DirCryptFile, false, Encoding.UTF8))
+                    content = srDecrypt.ReadLine();
+                    if (content != null)
                     {
-                        while (true)
-                        {
-                            content = srDecrypt.ReadLine();
-                            if (content != null)
-                            {
-                                swCrypt.WriteLine(CryptoWithoutTry.Encrypt(content, key));
-                            }
-                            else
-                            {
-                                srDecrypt.Close();
-                                swCrypt.Flush();
-                                swCrypt.Close();
-                                break;
-                            }
-                        }
-
+                        swCrypt.WriteLine(CryptoWithoutTry.Encrypt(content, key));
+                    }
+                    else
+                    {                                
+                        break;
                     }
                 }
                 return I_INPUTOUTPUTMESSAGE.Ok;
@@ -170,11 +164,19 @@ namespace ConsoleCrypt
                 ShowAPersone(message);
 #endif
             }
+            finally
+            {
+                srDecrypt?.Close();
+                swCrypt?.Flush();
+                swCrypt?.Close();
+            }
             return I_INPUTOUTPUTMESSAGE.ExceprionCryptFile;
         }
 
         public I_INPUTOUTPUTMESSAGE DecryptFile(string key)
         {
+            StreamReader srCrypt = null;
+            StreamWriter swDecrypt = null;
             try
             {
                 if (appSettings == null)
@@ -188,28 +190,21 @@ namespace ConsoleCrypt
                 if (String.IsNullOrWhiteSpace(key) || String.IsNullOrEmpty(key))
                     return I_INPUTOUTPUTMESSAGE.KeyIsNull;
                 string content;
-                using (StreamReader srCrypt = new StreamReader(appSettings.DirCryptFile, Encoding.UTF8))
-                {
-                    using (StreamWriter swDecrypt = new StreamWriter(appSettings.DirCryptFile, false, Encoding.UTF8))
-                    {
-                        while (true)
-                        {
-                            content = srCrypt.ReadLine();
-                            if (content != null)
-                            {
-                                swDecrypt.WriteLine(CryptoWithoutTry.Decrypt(content, key));
-                            }
-                            else
-                            {
-                                srCrypt.Close();
-                                swDecrypt.Flush();
-                                swDecrypt.Close();
-                                break;
-                            }
-                        }
 
+                srCrypt = new StreamReader(appSettings.DirCryptFile, Encoding.UTF8);
+                swDecrypt = new StreamWriter(appSettings.DirDecryptFile, false, Encoding.UTF8);
+                while (true)
+                {
+                    content = srCrypt.ReadLine();
+                    if (content != null)
+                    {
+                        swDecrypt.WriteLine(CryptoWithoutTry.Decrypt(content, key));
                     }
-                }
+                    else
+                    {                        
+                        break;
+                    }
+                }                 
                 return I_INPUTOUTPUTMESSAGE.Ok;
             }
             catch (Exception ex)
@@ -223,12 +218,18 @@ namespace ConsoleCrypt
                 ShowAPersone(message);
 #endif
             }
+            finally
+            {
+                srCrypt?.Close();
+                swDecrypt?.Flush();
+                swDecrypt?.Close();
+            }
             return I_INPUTOUTPUTMESSAGE.ExceprionDecryptFile;
         }
 
         public I_INPUTOUTPUTMESSAGE SearchBlockFromCryptRepositoriesUseKeyWord(string key, string keyWord, bool caseSensitive)
         {
-            
+            StreamReader srCrypt = null;
             try
             {
                 if (appSettings == null)
@@ -242,43 +243,42 @@ namespace ConsoleCrypt
                 if (String.IsNullOrWhiteSpace(key) || String.IsNullOrEmpty(key))
                     return I_INPUTOUTPUTMESSAGE.KeyIsNull;
                 string content, block="";
-                using (StreamReader srCrypt = new StreamReader(appSettings.DirCryptFile, Encoding.UTF8))
+                srCrypt = new StreamReader(appSettings.DirCryptFile, Encoding.UTF8);
+                bool flagTargetBlock = false;
+                if (!caseSensitive)
+                    keyWord = keyWord.ToLower();
+                while (true)
                 {
-                    bool flagTargetBlock = false;
-                    if (!caseSensitive)
-                        keyWord = keyWord.ToLower();
-                    while (true)
+                    content = srCrypt.ReadLine();
+                    if (content != null)
                     {
-                        content = srCrypt.ReadLine();
-                        if (content != null)
+                        content = (CryptoWithoutTry.Decrypt(content, key));
+                        block += $"{content}\r\n";
+                        if (!caseSensitive)
+                            content = content.ToLower();
+                        if(String.Equals(content, appSettings.SeparateBlock))
                         {
-                            content = (CryptoWithoutTry.Decrypt(content, key));
-                            block += $"{content}\r\n";
-                            if (!caseSensitive)
-                                content = content.ToLower();
-                            if(String.Equals(content, appSettings.SeparateBlock))
+                            if(flagTargetBlock)
                             {
-                                if(flagTargetBlock)
-                                {
-                                    block = block.Replace(appSettings.SeparateBlock, "");
-                                    ShowAPersone(block);
-                                    srCrypt.Close();
-                                    break;
-                                }
-                                block = "";
+                                block = block.Replace(appSettings.SeparateBlock, "");
+                                ShowAPersone(block);
+                                
+                                break;
                             }
-                            else if(content.IndexOf(keyWord) > -1)
-                            {
-                                flagTargetBlock = true;
-                            }
+                            block = "";
                         }
-                        else
+                        else if(content.IndexOf(keyWord) > -1)
                         {
-                            ShowAPersone("found nothing");
-                            break;
+                            flagTargetBlock = true;
                         }
-                    }                   
-                }
+                    }
+                    else
+                    {
+                        ShowAPersone("found nothing");
+                        break;
+                    }
+                }                   
+                
                 return I_INPUTOUTPUTMESSAGE.Ok;
             }
             catch (Exception ex)
@@ -291,6 +291,10 @@ namespace ConsoleCrypt
 #if DEBUG
                 ShowAPersone(message);
 #endif
+            }
+            finally
+            {
+                srCrypt?.Close();
             }
             return I_INPUTOUTPUTMESSAGE.SearchBlockFromCryptRepositoriesUseKeyWord;
         }        
